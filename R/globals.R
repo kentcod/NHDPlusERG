57acf727829e1377e82b53acbbd6a77f830b39f0
kentcod <coddingk@gmail.com>
2023-06-19 14:42:46 -0400

    globals check and read packages

--- R/globals.R
+++ R/globals.R
@@ -1,8 +1,13 @@
-#' @import sf
-#' @import nhdplusTools
-#' @import ggplot2
-#' @importFrom sf st_read st_make_valid
+packages <- c("ggplot2", "sf", "nhdplusTools")
 
+# Install packages not yet installed
+installed_packages <- packages %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+  install.packages(packages[!installed_packages])
+}
+
+# Packages loading
+lapply(packages, library, character.only = TRUE)
 
 setwd("C:/Users/Kent Codding/Desktop/ERG Summer 2023")
 FIPS <- st_read("UScounties") #read county FIPS map provided by esri
