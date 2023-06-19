2855ace64afa2adfb1a3a33a0f35423ec3319cd7
kentcod <coddingk@gmail.com>
2023-06-19 11:26:15 -0400

    globals update

--- R/globals.R
+++ R/globals.R
@@ -1,6 +1,7 @@
-library(sf)
-library(nhdplusTools)
-library(ggplot2)
-setwd("C:/Users/Kent Codding/Desktop/ERG Summer 2023/Task 2")
+#' @import sf
+#' @import nhdplusTools
+#' @import ggplot2
+
+setwd("C:/Users/Kent Codding/Desktop/ERG Summer 2023")
 FIPS <- st_read("UScounties") #read county FIPS map provided by esri
 polygons <- st_make_valid(FIPS) #get rid of 1 anomaly
