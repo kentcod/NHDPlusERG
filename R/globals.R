e30a07b31a92cc82827c1fbb2f93e0921e073ded
kentcod <coddingk@gmail.com>
2023-06-19 11:43:56 -0400

    fix import sf bug

--- R/globals.R
+++ R/globals.R
@@ -1,6 +1,8 @@
 #' @import sf
 #' @import nhdplusTools
 #' @import ggplot2
+#' @importFrom sf st_read st_make_valid
+
 
 setwd("C:/Users/Kent Codding/Desktop/ERG Summer 2023")
 FIPS <- st_read("UScounties") #read county FIPS map provided by esri
